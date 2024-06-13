e9576475d983417c94bd2ceea68dd87268b06532
“aakriti <aakritikhanna206@gmail.com>
2024-06-13 22:58:38 +0530

    second commit

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import dotenv from "dotenv";
+import databaseConnection from "./config/database.js";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/userRoute.js";
+import tweetRoute from "./routes/tweetRoute.js";
+import cors from "cors";
+
+dotenv.config({
+    path:".env"
+})
+databaseConnection();
+const app = express(); 
+
+// middlewares
+
+app.get("/", (req, res) => { 
+    // get the passed query 
+    
+    res.send("hello"); 
+}); 
+app.use(express.urlencoded({
+    extended:true
+}));
+app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+    origin:"https://twitter-o7tn.vercel.app",
+    credentials:true
+}
+
+app.use(cors(corsOptions));
+
+// app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+// api
+app.use("/api/v1/user",userRoute);
+app.use("/api/v1/tweet", tweetRoute);
+ 
+
+app.listen(process.env.PORT,() => {
+    console.log(`Server listen at port ${process.env.PORT}`);
+})
